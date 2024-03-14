c540f90c21ffc970f4d78b54709cb0d16dea2472
bagusqif29 <62454984+bagusqif29@users.noreply.github.com>
2024-03-14 16:19:20 +0700

    Update main.py

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import streamlit as st
 import requests
 from bs4 import BeautifulSoup
 
+
+st.set_page_config(layout='wide')
 st.markdown("""
     <style>
     #MainMenu {visibility: hidden;}
@@ -19,46 +21,47 @@ with st.form("Search"):
     search = st.form_submit_button("Search")
 
     if search:
-        count = 0
-        list_link = []
+        
         set_link = set()
-        col1, col2 = st.columns(2)
+        d_list = [[]]
+        link_list = []
+        col1, col2, col3, col4 = st.columns(4)
         page = requests.get(f"https://unsplash.com/s/photos/{keyword}")
         soup = BeautifulSoup(page.content, 'lxml')
         rows = soup.find_all("div", class_ = "ripi6")
-        for row in rows:
-            figures = row.find_all("figure")
-            for figure in figures:
-                divs = soup.find_all('div', class_='MorZF')
-
-                for div in divs:
-                    # Dalam setiap 'div', cari semua elemen 'img'
-                    images = div.find_all('img')
-                    
-                    for img in images:
-                        # Dapatkan atribut 'srcset' dari setiap 'img'
-                        srcset = img.get('srcset')
-                        
-                        # Cetak atribut 'srcset' jika ada
-                        if srcset is not None and len(set_link) < 36:
-                            count+=1
-                            srcset_parts = srcset.split(", ")
-                            for part in srcset_parts:
-                                if '2000w' in part:
-                                    # Extract the URL part before ' 1800w'
-                                    image_url = part.split(" ")[0]
-                                    
-                                    set_link.add(image_url)
-        
-        st.title(f'Length of list_link: {len(set_link)}')
-        
-        # st.title(f'Length of ripi6: {len(rows)}')
-        # st.title(f'Length of figures: {len(figures)}')
-        # st.title(f'Length of morzl: {len(divs)}')
-        # st.title(f'Length of image: {len(images)}')
+        for j, row in enumerate(rows):
+            temp_list = []
+            # st.title('row ke-'+ str(j))
+            
+            figures = row.find_all("figure")  
+            for i in range(len(figures)-1):
+                
+                # st.title('figure ke-'+ str(i))
+                div = figures[i].find("div", class_="MorZF")
+                # st.write('morzf =')
+                
+                img = div.find("img")
+                srcset = img['srcset']
+                if srcset is not None:
+                    srcset_parts = srcset.split(", ")
+                    for part in srcset_parts:
+                        if '2000w' in part and 'https://plus' not in part:
+                            image_url = part.split(" ")[0]
+                            # st.write(image_url)
+                            set_link.add(image_url)
+                            # link_list.append(image_url)
+                            # temp_list.append(image_url)
+                
+                
+                
         for a, link in enumerate(set_link):
-            if a%2 == 0:
+            if a%4 == 0:
                 col1.image(link)
-            else:
+            elif a%4 == 1:
                 col2.image(link)
-                                    
+            elif a%4 == 2:
+                col3.image(link)
+            elif a%4 == 3:
+                col4.image(link)    
+            else:
+                st.write("ERROR")      
